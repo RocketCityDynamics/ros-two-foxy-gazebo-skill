042b5d728b2eda4b1c16e378bf619afce20ae697
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-03-01 18:56:22 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,16 @@ class RosTwoFoxyGazebo(MycroftSkill):
     @intent_file_handler('gazebo.foxy.two.ros.intent')
     def handle_gazebo_foxy_two_ros(self, message):
         self.speak_dialog('gazebo.foxy.two.ros')
-        subprocess.call(["source /opt/ros/foxy/setup.bash"],shell=True)
-        subprocess.call([". ~/ros2_foxy/install/setup.bash"],shell=True)
-        subprocess.call(["gazebo /opt/ros/foxy/share/gazebo_plugins/worlds/gazebo_ros_diff_drive_demo.world"],shell=True)
+        #ros2
+        #subprocess.call(["source /opt/ros/foxy/setup.bash"],shell=True)
+        #subprocess.call([". ~/ros2_foxy/install/setup.bash"],shell=True)
+        #subprocess.call(["gazebo /opt/ros/foxy/share/gazebo_plugins/worlds/gazebo_ros_diff_drive_demo.world"],shell=True)
+        #ros1 for testing
+        subprocess.call(["source /opt/ros/noetic/setup.bash"],shell=True)
+        subprocess.call(["source ~/catkin_ws/devel/setup.bash"],shell=True)
+        subprocess.call(["roscore & rosrun gazebo_ros gazebo"],shell=True)
 
+        
 def create_skill():
     return RosTwoFoxyGazebo()
 
